a4db830c6d1c9565669207bae8e001a81d848400
유정민 <63496908+tyrosine1153@users.noreply.github.com>
2020-07-02 17:27:49 +0900

    0702

--- ID확인.c
+++ ID확인.c
@@ -3,18 +3,19 @@
 
 int main() {
 	char email[100];
-	int t,a=0,no=0;
+	int t, a = 0, no = 0, i, j;
 
 	scanf("%d", &t);
 	
-	for (int i = 0; i < t; i++) {
+	for (i = 0; i < t; i++) {
 		scanf("%s", email);
 	
-		for (int j = 0; email[j] != NULL; j++) {//틀린 이메일을 탐색하는 for문
+		for (j = 0; email[j] != NULL; j++) {//틀린 이메일을 탐색하는 for문
 			if (email[j] == '@') { a++; }//@의 개수
 			if ((email[j] >= 64 && email[j] <= 90) || (email[j] >= 97 && email[j] <= 122) || (email[j] >= 48 && email[j] <= 57) || email[j] == 46 || email[j] == 46) {}
 			else { no++; }//조건에 맞지않는 문자의 수
 		}
+		if (email[j - 1] == '@' || email[0] == '@') { no++; }
 		
 		if (a == 1) {
 			if (no == 0) {
